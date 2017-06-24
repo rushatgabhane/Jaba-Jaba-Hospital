84e53eb0c2bb907b72bf249a732505b1a0282b88
Mr.FuriousFury <amal110100@gmail.com>
2017-06-24 21:25:14 +0300

    borders.h working & simple encryption done

--- noname00.cpp
+++ noname00.cpp
@@ -1,45 +1,38 @@
 #include <iostream.h>
 #include <stdio.h>
 #include <conio.h>
-
+#include <string.h>
+#include "borders.h"
 //Function prototypes
 void getdoctor();       //To assign a doctor to a patient
 void addPatient();      //To add a patient
 void removePatient();   //To archive patient records
 void menu();            //To output the main menu
-void encrypt();         //To encrypt passwords
-void login(); 			//To login
+void encrypt(char);      //To encrypt passwords
+void login(); 				//To login
 void billing();			//Billing Function
-void borders2(int);		//To select different types of borders in a single function
+void borders(int);		//To select different types of borders in a single function
 
-void borders()
- {
-  	//Borders
-	for(int j=0;j<81;j++)
+char* encrypt(char pass[])
+{
+  int i=0;
+  for(i=0;pass[i]!='\0';i++)
+	 pass[i]+=strlen(pass);
+  return pass;
+}
+void borders(int i)
+{
+	switch(i)
 	{
-	 gotoxy(j,0);
-	 cout<<'*';
+	  case 1 : simple('*');
+				  break;
+	  case 2 : simple('#');
+				  break;
 	}
-	for(int k=1;k<24;k++)
-	{
-	 gotoxy(0,k);
-	 cout<<'*';
-	}
-	for(int m=0;m<81;m++)
-	{
-	 gotoxy(m,24);
-	 cout<<'*';
-	}
-	for(int n=1;n<24;n++)
-	{
-	 gotoxy(81,n);
-	 cout<<'*';
-	}
-	//Border End
- }
+}	  //Border End
  void login() //Our first function
  {
-	borders();
+	borders(1);
 	int i = 0;
 	char pass[200],uname[200];
 
@@ -62,15 +55,18 @@ void borders()
 	pass[i]='\0';
 	//Remove Output
 	clrscr();
-	borders();
+	borders(2);
 	gotoxy(30,10);
 	cout<<"The Username:"<<uname;
 	gotoxy(30,12);
 	cout<<"The Password:"<<pass;
+	gotoxy(30,14);
+	strcpy(pass,encrypt(pass));
+	cout<<"The Encrypted Pass: "<<pass;
  }
  void main()
  {
-  borders();
+  borders(1);
   login();
   getche();
  }
