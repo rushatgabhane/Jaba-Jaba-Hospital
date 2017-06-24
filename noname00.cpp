40cf96cd67ab94986101b5570694eb0565425b18
AlvinAntony <aaalvinantony@gmail.com>
2017-06-24 16:58:29 +0300

    Commited Again

--- noname00.cpp
+++ noname00.cpp
@@ -1,6 +1,9 @@
-#include<iostream.h>
- void main()
+#include<iostream>
+ int main
 {
  cout<<"\nHI";
- cout<<"This is so cool"
+ cout<<"This is so cool";
+ cout<<"\nGot Good C++ For 64-Bit";
+ return 0;
 }
+
