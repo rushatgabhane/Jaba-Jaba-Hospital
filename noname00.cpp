c1ac228cd4d1a29148e8465b65a39f722109f11a
Mr.FuriousFury <amal110100@gmail.com>
2017-06-24 17:05:01 +0300

    v1.0 of login added

--- noname00.cpp
+++ noname00.cpp
@@ -1,6 +1,29 @@
-#include<iostream.h>
+#include <iostream.h>
+#include <stdio.h>
+#include <conio.h>
+void login() //Our first function
+{
+	int i = 0;
+	char pass[200],uname[200];
+	cout<<"Enter username: ";
+	gets(uname);
+	cout<<"Enter password: ";
+	while(1)  //stars out password
+	{
+		pass[i] = getch();
+		if(pass[i]==13)
+		{
+		  pass[i]='\0';
+		  break;
+		}
+		cout<<'*';
+		i++;
+	}
+	pass[i]='\0';
+	cout<<'\n'<<pass;
+	cout<<'\n'<<uname;
+}
  void main()
 {
- cout<<"\nHI";
- cout<<"This is so cool";
+ login();
 }
