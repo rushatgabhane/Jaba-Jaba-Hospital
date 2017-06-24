0efbde3ecda611adf7468f0beafcd4288ee3f109
AlvinAntony <aaalvinantony@gmail.com>
2017-06-24 19:37:37 +0300

    Finished Alligning Everything

--- noname00.cpp
+++ noname00.cpp
@@ -5,8 +5,32 @@ void login() //Our first function
 {
 	int i = 0;
 	char pass[200],uname[200];
+	//Borders
+	for(int j=0;j<81;j++)
+	{
+	 gotoxy(j,0);
+	 cout<<'*';
+	}
+	for(int k=1;k<24;k++)
+	{
+	 gotoxy(0,k);
+	 cout<<'*';
+	}
+	for(int m=0;m<81;m++)
+	{
+	 gotoxy(m,24);
+	 cout<<'*';
+	}
+	for(int n=1;n<24;n++)
+	{
+	 gotoxy(81,n);
+	 cout<<'*';
+	}
+   //Border End
+	gotoxy(30,10);
 	cout<<"Enter username: ";
 	gets(uname);
+	gotoxy(30,12);
 	cout<<"Enter password: ";
 	while(1)  //stars out password
 	{
@@ -20,6 +44,7 @@ void login() //Our first function
 		i++;
 	}
 	pass[i]='\0';
+   //Remove Output
 	cout<<"\nThe Username:"<<uname;
    cout<<"\nThe Password:"<<pass;
 }
