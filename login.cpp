0ed8bb34bdb989928cb1d5e5a1cf2b3430ee9664
Mr.FuriousFury <amal110100@gmail.com>
2017-06-27 14:53:47 +0300

    login identation

--- login.cpp
+++ login.cpp
@@ -1,7 +1,7 @@
-#include<iostream.h>
-#include<conio.h>
-#include<stdio.h>
-#include<fstream.h>
+#include <iostream.h>
+#include <conio.h>
+#include <stdio.h>
+#include <fstream.h>
   class Hospital
   {
 	int idno;
