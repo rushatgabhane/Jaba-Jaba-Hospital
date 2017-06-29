19523c559c8968e616a6b399ee654a5a38165ce8
Dixit Sabharwal <dixitsabharwal05@gmail.com>
2017-06-29 12:57:12 +0300

    Update borders.h

--- borders.h
+++ borders.h
@@ -1,24 +1,26 @@
 //Simple border function
-void simple(char ch)
-{
-  for(int j=0;j<81;j++)
-  {
-	gotoxy(j,0);
-	cout<<ch;
-  }
-  for(int k=1;k<24;k++)
-  {
-	gotoxy(0,k);
-	cout<<ch;
-  }
-  for(int m=0;m<81;m++)
-  {
-	gotoxy(m,24);
-	cout<<ch;
-  }
-  for(int n=1;n<24;n++)
-  {
-	gotoxy(81,n);
-	cout<<ch;
-  }
-}
+void border(char ch,int x,int y,int l,int b) 
+//(x,y) is the top left corner point and l and b are the length and breadth of the rectangle
+{	
+	int p,q,r,s;
+	for(p=x;p<x+l;p++)
+	{
+		gotoxy(p,y);
+		cout<<ch;
+		}
+	for(q=y,--p;q<y+b;q++)
+	{
+		gotoxy(p,q);
+		cout<<ch;
+		}
+	for(r=x+l-1,--q;r>=x;r--)
+	{
+		gotoxy(r,q);
+		cout<<ch;
+		}
+	for(s=y+b-1;s>=y;s--)
+	{
+		gotoxy(x,s);
+		cout<<ch;
+		}
+    }
