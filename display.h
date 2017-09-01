0efe69bdf1b3d3dbde95437a885a2dd265395b21
Mr.FuriousFury <amal110100@gmail.com>
2017-09-01 12:27:13 +0300

    Fixed numbers disappearing from menu

--- display.h
+++ display.h
@@ -47,10 +47,14 @@ void dispArray(char* array[],int arraySize,int step=3,int start=6)
 {
   for(int i=0; i<arraySize;i++)
   {
-	 center(array[i],start+(i*step));
+    char word[50] = {i+49,'.'};
+    for(int j = 2; array[i][j-2]!='\0';j++)
+    {
+      word[j]=array[i][j-2];
+    }
+   center(word,start+(i*step));
   }
 }
-
 void createMenu(char* word,char* array[],int arraySize,int step=3)
 {
   clrscr();
