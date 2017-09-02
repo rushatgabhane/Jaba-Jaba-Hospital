3cf596dbb29d467c216d72c104638f99713e870f
Mr.FuriousFury <amal110100@gmail.com>
2017-09-02 12:57:18 +0300

    some wierd error

--- display.h
+++ display.h
@@ -1,90 +1,90 @@
 //Creates horizontal rule
 void hr(int i,char ch)
 {
-  for(int j=0;j<81;j++)
-  {
-    gotoxy(j,i);
-    cout<<ch;
-  }
+	for(int j=0;j<81;j++)
+	{
+		gotoxy(j,i);
+		cout<<ch;
+	}
 }
 //Creates Vertical Rule
 void vr(int i,char ch)
 {
-  for(int j=0;j<25;j++)
-  {
-    gotoxy(i,j);
-    cout<<ch;
-  }
+	for(int j=0;j<25;j++)
+	{
+		gotoxy(i,j);
+		cout<<ch;
+	}
 }
 //Creates borders
 void borders()
 {
-  char ch = '*';
-  hr(0,ch);
-  hr(25,ch);
-  vr(0,ch);
-  vr(81,ch);
+	char ch = '*';
+	hr(0,ch);
+	hr(25,ch);
+	vr(0,ch);
+	vr(81,ch);
 }
 
 //Center Functions
 void center(char* word,int y=12.5)
 {
-  int length = strlen(word);
-  gotoxy(40.5 - length/2,y);
-  cout<<word;
+	int length = strlen(word);
+	gotoxy(40.5 - length/2,y);
+	cout<<word;
 }
 
 //Display Menu
 /*void createMenu(char* word)
 {
-  clrscr();
-  borders();
-  hr(4,'*');
-  center(word,2);
+	clrscr();
+	borders();
+	hr(4,'*');
+	center(word,2);
 }*/
 
 void dispArray(char* array[],int arraySize,int step=3,int start=6)
 {
-  for(int i=0; i<arraySize;i++)
-  {
-    char word[50] = {i+49,'.'};
-    for(int j = 2; array[i][j-2]!='\0';j++)
-    {
-      word[j]=array[i][j-2];
-    }
-   center(word,start+(i*step));
-  }
+	for(int i=0; i<arraySize;i++)
+	{
+		char word[50] = {i+49,'.'};
+		for(int j = 2; array[i][j-2]!='\0';j++)
+		{
+			word[j]=array[i][j-2];
+		}
+	 center(word,start+(i*step));
+	}
 }
 //Menu functions
 //Creates a menu with the given heading and the elements passed through the array
 void createMenu(char* word,char* array[],int arraySize,int step=3)
 {
-  clrscr();
-  borders();
-  hr(4,'*');
-  center(word,2);
-  dispArray(array,arraySize,step,6);
+	clrscr();
+	borders();
+	hr(4,'*');
+	center(word,2);
+	dispArray(array,arraySize,step,6);
 }
 //Creates output screen with border and heading
 void createMenu(char* word)
 {
-  clrscr();
-  borders();
-  hr(4,'*');
-  center(word,2);
+	clrscr();
+	borders();
+	hr(4,'*');
+	center(word,2);
 }
 void errormsg(char* error="null")
 {
-  clrscr();
-  borders();
-  if(strcmp(error,"null"))
-    center(error);
-  center("Press any key to continue...",17);
-  getche();
+	clrscr();
+	borders();
+	if(strcmp(error,"null"))
+		center(error);
+	center("Press any key to continue...",17);
+	getche();
 }
 //push the output in middle of screen, left alignment 
 void Lalign(char* text,int y=7)
 {
-  gotoxy(35,y);
-  cout<<text;
+	gotoxy(35,y);
+	cout<<text;
 }
